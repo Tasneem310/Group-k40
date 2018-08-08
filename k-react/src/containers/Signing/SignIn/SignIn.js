587eb4ca2faa9a57f1485cc464c8211dc3ed704c
Tasneem310 <41957155+Tasneem310@users.noreply.github.com>
2018-08-08 11:59:10 +0300

    edit on sign in page

--- k-react/src/containers/Signing/SignIn/SignIn.js
+++ k-react/src/containers/Signing/SignIn/SignIn.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 const SignIn = () => (
     <React.Fragment>
-        
+        <div>
+        <h2>Sign In</h2>
+        <form onSubmit={#}>
+          <FormGroup bsSize="large">
+            <ControlLabel>Email or Username</ControlLabel>
+            <FormControl autoFocus type="Email or Username" />
+          </FormGroup>
+          <FormGroup bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              value={this.state.password} type="Password" />
+          </FormGroup>
+          <Button block bsSize="large" type="submit" >
+            Login
+          </Button>
+        </form>
+        <p>Not a member?</p>
+        <Link to = "/SignUp"><p>SIGN UP</p></Link>
+      </div>
     </React.Fragment>
 );
 
