fbad88dda9267145dfc0ac90e32703e3e87ff38e
سلطان الديحاني <smdssaj@gmail.com>
2018-08-08 13:52:13 +0300

    تسجيل جديد مبدئي

--- k-react/src/containers/Signing/SignUp/SignUp.js
+++ k-react/src/containers/Signing/SignUp/SignUp.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { Component } from "react";
 
-const SignUp = () => (
-    <React.Fragment>
+import classes from "./SignUp.css"
 
-    </React.Fragment>
-);
+class SignUp extends Component {
+  state = {
+    sName: "",
+    pass: "",
+    rePass: "",
+    eMail: ""
+  };
+  nameHandler = event => {
+    this.setState({ sName: event.value });
+  };
+  passHandler = event => {
+    this.setState({ pass: event.value });
+  };
+  rePassHandler = event => {
+    this.setState({ rePass: event.value });
+  };
+  eMailHandler = event => {
+    this.setState({ eMail: event.value });
+  };
+  render() {
+    return (
+      <center>
+        student name:<br />
+        <input
+          type="text"
+          onChange={this.nameHandler}
+          value={this.state.sName}
+        />
+        <br />
+        password:<br />
+        <input
+          type="text"
+          onChange={this.passHandler}
+          value={this.state.pass}
+        />
+        <br />
+        re-enter password:<br />
+        <input
+          type="text"
+          onChange={this.rePassHandler}
+          value={this.state.rePass}
+        />
+        <br />
+        e-mail:<br />
+        <input
+          type="text"
+          onChange={this.eMailHandler}
+          value={this.state.eMail}
+        />
+        <br />
+        <br />
+        <input type="submit" value="Submit" />
+      </center>
+    );
+  }
+}
 
 export default SignUp;
